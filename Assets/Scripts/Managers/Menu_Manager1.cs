7479bf6ac0f65eb8fa336267215b4148534a6b9e
MATU <82177532+MATUSTRM@users.noreply.github.com>
2026-06-08 22:36:23 -0400

    Transition
    *Transition esta funcionando y ahora falta implementar el delay

--- Assets/Scripts/Managers/Menu_Manager1.cs
+++ Assets/Scripts/Managers/Menu_Manager1.cs
@@ -1,40 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+    //ESTE SCRIPT SE ENCARGARA DE MANEJAR LOS FLUJO DE PANTALLA DE LOS MENUS
+    //MEDIANTE LOS BOTONES Y CADA BOTON PODRA LLAMAR A UNA TRANSICION
 public class Menu_Manager1 : MonoBehaviour
 {
     private GameObject current;
     private GameObject next;
     [SerializeField] private List<ButtonInfo> buttons = new List<ButtonInfo>();
-    [SerializeField] private float delay_time;
+    
 
+
+
+    [SerializeField] Animator Main_transition;
     void Start()
     {
         for (int i = 0; i < buttons.Count; i++)
         {
             ButtonInfo info = buttons[i];
-
+            
             if (info.button != null && info.currentPanel != null && info.nextPanel != null)
             {
-                current = info.currentPanel;
-                next = info.nextPanel;
+                GameObject current = info.currentPanel;
+                GameObject next = info.nextPanel;
 
-                // Opcional: log para ver a dónde va
+    
                 string destino = info.destiny_name;
-
-                info.button.onClick.AddListener(() =>
+                info.button.onClick.AddListener(() =>{Debug.Log("Cambiando a: " + destino);ButtonAction(current, next);
+                 switch(info.transition)
                 {
-                    Debug.Log("Cambiando a: " + destino);
-                    ButtonAction();
-                });
+                    case ButtonInfo.transition_type.nothing:
+                        break;
+                    case ButtonInfo.transition_type.cine:
+                        Main_transition.Play("cine");
+                        break;
+                    case ButtonInfo.transition_type.flash:
+                        Main_transition.Play("flash");       
+                        break;
+                };});
+
+
+            if (Main_transition == null)
+            {
+                Debug.LogWarning("MAIN TRANSITION NO ASIGNADO");
+            }
+            
             }
         }
     }
 
 
-    private void ButtonAction()
+    private void ButtonAction(GameObject current, GameObject next)
     {
         current.SetActive(false);
         next.SetActive(true);
@@ -43,17 +60,23 @@ public class Menu_Manager1 : MonoBehaviour
     //HAY QUE TRANSICIONAR BIEN ENTRE LOS MENUS, MIENTRAS ESTE EN LA TRANSICION
     //NECESITO CREAR UN METODO SIN PARAMETRO QUE SEA PUBLICO, SE AGREGARA COMO EVENTO EN LA ANIMACION DE TRANSICIONES
 
-    public void Next_menu()
+    //Estaba probando si era mejor el codigo asi, pero queda mejor 
+    public void checK_transition()
     {
-        ButtonAction();
+            
     }
+
 }
 
 [System.Serializable]
 public class ButtonInfo
 {
     public string destiny_name; 
+    public enum transition_type {nothing,cine,flash}
+    public transition_type transition;
     public Button button;
+
+    [Range(0,5)] public float delay_time;
     public GameObject currentPanel;
     public GameObject nextPanel;
 }
