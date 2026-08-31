03bd28727004a724a6b79651af8470aa640db2ef
MATU <82177532+MATUSTRM@users.noreply.github.com>
2026-08-30 21:54:25 -0400

    sistema de cambiar sede con horario funciona

--- Assets/Scripts/Mostrar_sedes.cs
+++ Assets/Scripts/Mostrar_sedes.cs
@@ -6,14 +6,18 @@ public class Mostrar_sedes : MonoBehaviour
 {
     public TMP_Dropdown Opciones;
     public TextMeshProUGUI Text_descripcion;
-    public RawImage imagen;
+    public Image imagen;
     public Sedes[] sedes;
+
+    public GameObject[] horarios;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Opciones.onValueChanged.AddListener(cambiar_sede);
         cambiar_sede(Opciones.value);
+        Opciones.onValueChanged.AddListener(cambiar_horario);
+        cambiar_horario(Opciones.value);
     }
 
     public void cambiar_sede(int index)
@@ -34,15 +38,35 @@ public class Mostrar_sedes : MonoBehaviour
         //OBTENEMOS LA IMAGEN DE LA SEDE MEDIANTE EL MISMO SCRIPTABLE OBJECT
         if (sedes[index].imagen_sede != null)
         {
-            imagen.texture = sedes[index].imagen_sede;
+            imagen.sprite = sedes[index].imagen_sede;
         }
         else
         {
-            imagen.texture = sedes[index].imagen_sede;
+            imagen.sprite = sedes[index].imagen_sede;
         }
         
 
     }
+
+
+    public void cambiar_horario(int index)
+    {
+        foreach (var horario in horarios)
+        {
+            //SI EL INDEX ES IGUAL AL INDICE DE HORARIOS
+            if (horarios[index] != horario)
+            {
+                //ACTIVA EL GAMEOBJECT
+                horario.SetActive(false);
+            }
+            else
+            {
+                horario.SetActive(true);
+            }
+        }
+    }
+
+
 }
 
 
